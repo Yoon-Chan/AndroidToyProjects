d33307031445da8e0e3d8b8ff9664b236eb76bf9
윤찬 <vsvx13@naver.com>
2025-07-29 16:36:49 +0900

    [Oauth2] Server 프로젝트 초기화

---
+++ oauthEx/oauth-server/oauth-server/src/test/java/com/chan/oauthserver/OauthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chan.oauthserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OauthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
