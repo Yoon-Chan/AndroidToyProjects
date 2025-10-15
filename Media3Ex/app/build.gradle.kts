effb37f64cc6eaa62a807d71f3ecc4c978943414
윤찬 <vsvx13@naver.com>
2025-10-15 22:09:25 +0900

    [feat][media3] java version 17, add library hilt, ksp, circuit, parcelize
    자바 버전 17 변경과 관련 라이브러리들 설정(hilt, ksp, circuit, parcelize)

--- Media3Ex/app/build.gradle.kts
+++ Media3Ex/app/build.gradle.kts
@@ -2,6 +2,9 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
+    alias(libs.plugins.hilt.plugin)
+    id("kotlin-parcelize")
 }
 
 android {
@@ -28,14 +31,17 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
     buildFeatures {
         compose = true
+    }
+    ksp {
+        arg("circuit.codegen.mode", "hilt")
     }
 }
 
@@ -56,4 +62,13 @@ dependencies {
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
+
+    //hilt
+    implementation(libs.dagger.hilt.android)
+    ksp(libs.dagger.hilt.compiler)
+
+    //circuit
+    implementation(libs.bundles.circuit)
+    api(libs.slack.circuit.codegen.annotations)
+    ksp(libs.slack.circuit.codegen)
 }
